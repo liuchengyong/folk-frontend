e0d74df46cef720e24679ad74bed0a7f711cf311
barlowluo <luoweime@gmail.com>
2016-02-26 15:43:05 +0800

    [Luowei] configured test env

--- src/config/test.js
+++ src/config/test.js
@@ -3,7 +3,9 @@
 import baseConfig from './base';
 
 let config = {
-  appEnv: 'test'  // don't remove the appEnv property here
+  appEnv: 'test',  // don't remove the appEnv property here
+  apiUrl: 'http://123.57.174.208:8080',
+  baseUrl: 'http://wetest.zhid58.com'
 };
 
 export default Object.freeze(Object.assign(baseConfig, config));
