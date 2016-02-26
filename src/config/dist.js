a10725a156ecd411e94369c46a266bc52cf499ec
barlowluo <luoweime@gmail.com>
2016-02-26 15:53:24 +0800

    [Luowei] added dist baseUrl

--- src/config/dist.js
+++ src/config/dist.js
@@ -3,7 +3,9 @@
 import baseConfig from './base';
 
 let config = {
-  appEnv: 'dist' // feel free to remove the appEnv property here
+  appEnv: 'dist', // feel free to remove the appEnv property here
+  apiUrl: 'http://123.57.174.208:8080',
+  baseUrl: 'http://wetest.zhid58.com'
 };
 
 export default Object.freeze(Object.assign({}, baseConfig, config));
