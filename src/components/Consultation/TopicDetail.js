16a97c6c4d05ba0cc5f35696d828a39002375b6d
barlowluo <luoweime@gmail.com>
2016-03-12 17:30:04 +0800

    [Luowei] added topic detail link

--- src/components/Consultation/TopicDetail.js
+++ src/components/Consultation/TopicDetail.js
@@ -3,33 +3,36 @@
  */
 import React from 'react';
 import config from 'config';
+import {Link} from 'react-router';
 import defaultIcon from '../../images/icon/logo_icon.png';
 import icPingLun from '../../images/ic_pinglun.png'
 
 const TopicDetail = (props) => (
-  <div className='talk-item'>
-    <div className='talk-head'>
+
+  <Link to={`/main/topic/${props.topic.id}`}>
+    <div className='talk-item'>
+      <div className='talk-head'>
         <span className='person-headimg'>
             <img className='topics_avatar' src={props.expert.user.avatar || defaultIcon}/>
         </span>
-      <div className='personbox'>
-        <span className='topics_name'>{props.expert.user.loginName || props.expert.user.name || '匿名'}</span>
-        <span>｜</span>
-        <span className='topics_title'>{props.topic.title}</span>
+        <div className='personbox'>
+          <span className='topics_name'>{props.expert.user.loginName || props.expert.user.name || '匿名'}</span>
+          <span>｜</span>
+          <span className='topics_title'>{props.topic.title}</span>
+        </div>
       </div>
-
-    </div>
-    <div className='talk-body'>
-      <span className='tag topics_role'>{config.roles[props.expert.expert.role]}</span>
-      <span className='talk topics_description'>{props.topic.description}</span>
-    </div>
-    <div className='talk-foot'>
-      <img className='answerimg' src={icPingLun}/>
+      <div className='talk-body'>
+        <span className='tag topics_role'>{config.roles[props.expert.expert.role]}</span>
+        <span className='talk topics_description'>{props.topic.description}</span>
+      </div>
+      <div className='talk-foot'>
+        <img className='answerimg' src={icPingLun}/>
       <span className='answenum'>
         <span className='topics_countOfComment'>{props.countOfComment}</span>
       </span>
+      </div>
     </div>
-  </div>
+  </Link>
 );
 
 export default TopicDetail;
