8d1a3db03093e09f1070fe3a43e1498b451249e7
allen-blue <polandeme@gmail.com>
2016-03-18 14:56:31 +0800

    [HGR] new loading.fig

--- src/components/Common/Loading.js
+++ src/components/Common/Loading.js
@@ -7,8 +7,6 @@ import React from 'react';
 require('styles/_loading.scss');
 let loading = require('../../images/loading.gif');
 
-// import CircularProgress from 'material-ui/lib/circular-progress';
-
 const Loading = () => (
   <div className="loading-wrap">
   	<img src={loading} />
