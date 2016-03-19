c4cd36b18f5fcf2579ba5871dd11c3866ee643c4
barlowluo <luoweime@gmail.com>
2016-03-19 21:07:02 +0800

    [Luowei] changed url

--- src/config/dev.js
+++ src/config/dev.js
@@ -4,8 +4,8 @@ Object.assign = require('object-assign');
 
 let config = {
   appEnv: 'dev',  // feel free to remove the appEnv property here
-  apiUrl: 'http://test.zhid58.com:8080',
-  baseUrl: 'http://wetest.zhid58.com'
+  apiUrl: 'http://www.zhid58.com:8080',
+  baseUrl: 'http://www.zhid58.com'
 };
 
 export default Object.assign({}, baseConfig, config);
