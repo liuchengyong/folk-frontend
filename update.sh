aa65cdbe4282b30506e18d20a61709d117cac0ef
barlowluo <luoweime@gmail.com>
2016-03-21 11:24:20 +0800

    [Luowei] updated update.sh

--- update.sh
+++ update.sh
@@ -2,10 +2,10 @@
 git pull origin master 
 cnpm install 
 rm -rf ./dist/assets/*
-if ["$NODE_ENV" = "production"]
-then 
-  npm run dist
-else 
-  npm run uat
+if [ "$NODE_ENV" = "production" ];
+  then 
+    npm run dist
+  else 
+    npm run uat
 fi
 npm run cnd
