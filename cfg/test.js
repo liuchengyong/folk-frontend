0402d0c6f29034a5b6aca8c5c51304e099e04e49
诚勇刘 <liuchengyong@iluoteng.com>
2016-06-22 14:19:22 +0800

    移除wetest.zhid58.com 的数据统计

--- cfg/test.js
+++ cfg/test.js
@@ -35,7 +35,7 @@ let config = Object.assign({}, baseConfig, {
     new HtmlWebpackPlugin({
       filename: '../main.html',
       title: '指点我吧',
-      template: 'index-tmp.ejs'
+      template: 'index-tmp-test.ejs'
     })
   ],
   module: defaultSettings.getDefaultModules()
