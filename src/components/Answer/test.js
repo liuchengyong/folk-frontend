514f5259cbdb8d01cd524a132430a5cc00456321
诚勇刘 <liuchengyong@iluoteng.com>
2016-06-29 21:22:59 +0800

    no message

--- src/components/Answer/test.js
+++ src/components/Answer/test.js
@@ -62,15 +62,16 @@ class AnswerComponent extends React.Component {
     this.fetchOrderConfig(this.props.user,this.props.answer.answer.answerId,this.props.answer.answer.question.title)
     .then(response => response.json())
     .then(data => {
-      data = data.param;
+      let paySign = data.param.paySign;
+      data = data.param.response;
       console.log(data);
       wx.chooseWXPay({
-          appId:'wx254dcfe98729df4b',
+          appId:data.appid,
           timestamp: data.myTimestamp, // 支付签名时间戳，注意微信jssdk中的所有使用timestamp字段均为小写。但最新版的支付后台生成签名使用的timeStamp字段名需大写其中的S字符
-          nonceStr: data.nonce_str, // 支付签名随机串，不长于 32 位
+          nonceStr: data.myNoncestr, // 支付签名随机串，不长于 32 位
           package: `prepay_id=${data.prepay_id}`, // 统一支付接口返回的prepay_id参数值，提交格式如：prepay_id=***）
           signType: 'MD5', // 签名方式，默认为'SHA1'，使用新版支付需传入'MD5'
-          paySign: data.mySign, // 支付签名
+          paySign: paySign, // 支付签名
           success: function (res) {
               alert(res);
           }
