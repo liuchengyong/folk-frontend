fe8eff0fc41cff67511f338c793ef087095b7c55
诚勇刘 <liuchengyong@iluoteng.com>
2016-06-29 23:09:11 +0800

    no message

--- src/components/Answer/test.js
+++ src/components/Answer/test.js
@@ -65,33 +65,33 @@ class AnswerComponent extends React.Component {
     .then(data => {
       console.log(data);
       data = data.param;
-      // WeixinJSBridge.invoke(
-      //    'getBrandWCPayRequest', {
-      //       appId: data.appid,     //公众号名称，由商户传入 
-      //       nonceStr: data.myNoncestr, //随机串    
-      //       package:'prepay_id=' + data.prepay_id,    
-      //       signType: 'MD5',         //微信签名方式：    
-      //       timestamp:data.myTimestamp,         //时间戳，自1970年以来的秒数       
-      //       paySign: data.mySign //微信签名 
-      //    },
-      //    function(res){ 
-      //        alert(res);    
-      //        if(res.err_msg == 'get_brand_wcpay_request：ok' ) {
-      //           alert("success");
-      //        }     // 使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返回    ok，但并不保证它绝对可靠。 
-      //    }
-      // ); 
+      WeixinJSBridge.invoke(
+         'getBrandWCPayRequest', {
+           "appId" ： "wx2421b1c4370ec43b",     //公众号名称，由商户传入     
+           "timeStamp"：" 1395712654",         //时间戳，自1970年以来的秒数     
+           "nonceStr" ： "e61463f8efa94090b1f366cccfbbb444", //随机串     
+           "package" ： "prepay_id=u802345jgfjsdfgsdg888",     
+           "signType" ： "MD5",         //微信签名方式：     
+           "paySign" ： "70EA570631E4BB79628FBCA90534C63FF7FADD89" //微信签名 
+         },
+         function(res){ 
+             alert(res);    
+             if(res.err_msg == 'get_brand_wcpay_request：ok' ) {
+                alert("success");
+             }     // 使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返回    ok，但并不保证它绝对可靠。 
+         }
+      ); 
 
-      wx.chooseWXPay({
-          timestamp: data.myTimestamp, // 支付签名时间戳，注意微信jssdk中的所有使用timestamp字段均为小写。但最新版的支付后台生成签名使用的timeStamp字段名需大写其中的S字符
-          nonceStr: data.myNoncestr, // 支付签名随机串，不长于 32 位
-          package: `prepay_id=${data.prepay_id}`, // 统一支付接口返回的prepay_id参数值，提交格式如：prepay_id=***）
-          signType: 'MD5', // 签名方式，默认为'SHA1'，使用新版支付需传入'MD5'
-          paySign: data.mySign, // 支付签名
-          success: function (res) {
-              alert(res);
-          }
-      });
+      // wx.chooseWXPay({
+      //     timestamp: data.myTimestamp, // 支付签名时间戳，注意微信jssdk中的所有使用timestamp字段均为小写。但最新版的支付后台生成签名使用的timeStamp字段名需大写其中的S字符
+      //     nonceStr: data.myNoncestr, // 支付签名随机串，不长于 32 位
+      //     package: `prepay_id=${data.prepay_id}`, // 统一支付接口返回的prepay_id参数值，提交格式如：prepay_id=***）
+      //     signType: 'MD5', // 签名方式，默认为'SHA1'，使用新版支付需传入'MD5'
+      //     paySign: data.mySign, // 支付签名
+      //     success: function (res) {
+      //         alert(res);
+      //     }
+      // });
     });
     
   }
