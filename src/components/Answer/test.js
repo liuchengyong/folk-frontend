22ee94b98ab344a3557925ae1bf15c398803d28b
诚勇刘 <liuchengyong@iluoteng.com>
2016-06-29 23:12:02 +0800

    no message

--- src/components/Answer/test.js
+++ src/components/Answer/test.js
@@ -67,12 +67,12 @@ class AnswerComponent extends React.Component {
       data = data.param;
       WeixinJSBridge.invoke(
          'getBrandWCPayRequest', {
-           "appId" ： "wx2421b1c4370ec43b",     //公众号名称，由商户传入     
-           "timeStamp"：" 1395712654",         //时间戳，自1970年以来的秒数     
-           "nonceStr" ： "e61463f8efa94090b1f366cccfbbb444", //随机串     
-           "package" ： "prepay_id=u802345jgfjsdfgsdg888",     
-           "signType" ： "MD5",         //微信签名方式：     
-           "paySign" ： "70EA570631E4BB79628FBCA90534C63FF7FADD89" //微信签名 
+           "appId": "wx2421b1c4370ec43b",     //公众号名称，由商户传入     
+           "timeStamp":" 1395712654",         //时间戳，自1970年以来的秒数     
+           "nonceStr" : "e61463f8efa94090b1f366cccfbbb444", //随机串     
+           "package" : "prepay_id=u802345jgfjsdfgsdg888",     
+           "signType" : "MD5",         //微信签名方式:     
+           "paySign" : "70EA570631E4BB79628FBCA90534C63FF7FADD89" //微信签名 
          },
          function(res){ 
              alert(res);    
